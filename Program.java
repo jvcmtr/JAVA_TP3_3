70aaede6c640bab9835b54a272a69e8367a2e70e
jvcmtr <jvictorcmtr@gmail.com>
2023-09-14 13:20:22 -0300

    DONE

--- Program.java
+++ Program.java
@@ -7,7 +7,8 @@ public class Program {
 
         if (args.length == 0){
             Scanner in = new Scanner(System.in);
-            System.out.println("Digite um nome : ");
+            System.out.println();
+            System.out.print("Digite um nome : ");
             s = in.nextLine();
             in.close();
         }
@@ -18,7 +19,15 @@ public class Program {
             s = s.trim();
         }
 
-        s.replaceAll("\\w[e(d[aeiou])]\\w", " ");
-        System.out.println(s);
+        String sigla = s.toUpperCase().replaceAll(" (E|(D(A|E|I|O|U))) ", " ");
+        String[] a = sigla.split(" ");
+        sigla = "";
+        for (String word : a) {
+            sigla += word.trim().charAt(0);
+        }
+        System.out.println("SIGLA : " + s);
+        System.out.println("\t" + sigla);
+        System.out.println("\n");
+
     }   
 }
